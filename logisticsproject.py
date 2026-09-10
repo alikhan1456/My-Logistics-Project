95f7f9b0c3c7da47f5949ea2d79a80b89c8e3328
Malak Muhammad Ali <malikalikhan0305@gmail.com>
2026-09-10 19:47:13 +0500

    Day 6 of python code

---
+++ logisticsproject.py
@@ -0,0 +1,124 @@
+drivers=["Zarak", "Ahmad", "Ali", "Khan", "Muhammad"]
+jobs=[]
+
+#________________________Pickup-Dropoff______________________________________
+def get_job_location():
+    pickup_location= input("Please enter the pickup-location:")
+    drop_location= input("Please enter the dropoff-location:")
+    return pickup_location, drop_location
+
+pickup_location, drop_location= get_job_location()
+
+#____________________________________________________________________________
+
+
+#________________________Date-Time___________________________________________
+def get_job_datetime():
+    pickup_datetime= input("Enter the pickup date and time:")
+    drop_datetime= input("Enter the droppoff date and time:")
+    return pickup_datetime,drop_datetime
+
+pickup_datetime, drop_datetime= get_job_datetime()
+#____________________________________________________________________________
+
+
+
+
+
+#________________________Customer Details____________________________________
+
+def customer_details():
+    customer_name= input("Please enter the customer name:")
+    customer_phone= input("Enter customer Phone number:")
+    customer_email= input("Enter customer email:")
+    return customer_name, customer_phone, customer_email
+
+customer_name,customer_phone,customer_email= customer_details()
+#____________________________________________________________________________    
+
+
+#__________________________Basic-job-info____________________________________
+def get_job_basic_info():
+    job_id= input("Please enter the job id:")
+    job_type= input("Enter job type, eg(House move, Cx job): ")
+    weight= float(input("Enter the weight of items:"))
+    notes= input("Enter Any notes here: ")
+    van_type= input("Enter the van type needed by customer:")
+    return job_id,job_type,weight,notes,van_type
+
+job_id,job_type,weight,notes,van_type= get_job_basic_info()
+
+#____________________________________________________________________________
+
+
+#_________________________Price-details______________________________________
+def get_price_details():
+    price= int(input("Enter the price agreed: "))
+    commission= (price*20)/100
+    return price, commission
+
+price, commission= get_price_details()
+
+#____________________________________________________________________________
+
+
+
+
+#_______________________Assign driver or not and status______________________
+def assign_driver():
+    driver= ""
+    status= "Unassigned"
+
+    question=input("Do you want the driver assigned?: ").lower()
+    if question=="yes":
+        driver= input("Enter the driver:")
+        if driver in drivers:
+            status= "Assigned"
+        else: 
+            driver=""
+            print("The driver is not in the list")
+    return driver, status
+
+driver, status= assign_driver()
+#____________________________________________________________________________
+
+
+
+job={
+    "pickup":pickup_location,
+    "pickup date/time": pickup_datetime,
+    "dropoff":drop_location,
+    "dropoff date/time": drop_datetime,
+    "job id": job_id,
+    "customer name": customer_name,
+    "customer phone": customer_phone,
+    "customer email": customer_email,
+    "job type": job_type,
+    "weight-items": weight,
+    "notes": notes,
+    "van type": van_type,
+    "price": price,
+    "commission": commission,
+    "driver":driver,
+    "status":status,
+
+}
+    
+jobs.append(job)    
+print(jobs)
+
+
+
+
+
+
+
+
+
+
+
+
+         
+
+
+
